d5b0973b1a1f25bc4565fff2272c875207a1852f
Chae Reen Kang <cherish4@g.skku.edu>
2023-07-12 11:27:41 +0900

    회귀식 오탈자 수정

--- heart.R
+++ heart.R
@@ -14,12 +14,31 @@ heart = c('heart_before1', 'heart_before2', 'heart_before3', 'heart_before4',
 org_dat %>%
   mutate(mean_heart = rowMeans(cur_data()[,heart], na.rm=T)) -> mean_dat
 
-heart_mod <- 'score ~ mean_heart
-              mean_heart ~ ind + A
-              ind ~~ age +female + rankinground_rank + worldranking
-              A ~~ set_number + arrows + shoot_order + countdown'
+heart_fa <- 'ind =~ age + female + rankinground_rank + worldranking
+              Arrow =~ set_number + arrows + shoot_order + countdown'
+fit_fa = cfa(heart_fa, data=mean_dat)
+summary(fit_fa, fit.measures = T, standardized = T)
+fit_fa %>% modificationindices() %>% arrange(mi)
+
+#측정변수로만
+heart_simp <- 'score ~ mean_heart + female + rankinground_rank + set_number + shoot_order
+              mean_heart ~ set_number + arrows + shoot_order + countdown + worldranking + rankinground_rank'
+
+fit_heart0 = sem(model = heart_simp, estimator = "ML", missing = "pairwise",
+                data = mean_dat, meanstructure = F)
+#varTable(fit_heart0)
+summary(fit_heart0, fit.measures = T, standardized = T)
+fit_heart0 %>% modificationindices() %>% arrange(mi)
+semPaths(fit_heart0, what = 'est', style = 'lisrel')
+
+####latent####
+heart_mod <- 'ind =~ age + female + rankinground_rank + worldranking
+              Arrow =~ set_number + arrows + shoot_order + countdown
+              score ~ mean_heart
+              mean_heart ~ ind + Arrow'
 
 fit_heart = sem(model = heart_mod, estimator = "ML", missing = "fiml",
                 data = mean_dat, meanstructure = T)
+varTable(fit_heart)
 summary(fit_heart, fit.measures = T, standardized = T)
 
