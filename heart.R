c79fe434ecfcd6471069c6d43dc57ec1b4d74124
Chae Reen Kang <cherish4@g.skku.edu>
2023-07-19 13:22:06 +0900

    writing model4 & adding model5

--- heart.R
+++ heart.R
@@ -13,7 +13,13 @@ heart = c('heart_before1', 'heart_before2', 'heart_before3', 'heart_before4',
           'heart_before9','heart_before10','heart_before11','heart_before12')
 org_dat %>%
   mutate(mean_heart = rowMeans(cur_data()[,heart], na.rm=T)) -> mean_dat
+write.csv(mean_dat,file="data2.csv")
+summary(lm(score ~ mean_heart,data=mean_dat))
+summary(lm(score ~ mean_heart + age + female + worldranking + rankinground_rank 
+           + stage + set_number + shoot_order + countdown + arrows,data=mean_dat))
+aa <- lm(score ~ mean_heart + factor(match_name_id),data=mean_dat)
 
+summary(aa)
 ##회귀식 하나씩##
 #model1
 model1 <- 'score ~ mean_heart'
@@ -27,9 +33,13 @@ fit2 <- sem(model = model2, estimator = "ML", missing = "fiml",
             data = mean_dat, meanstructure = T)
 summary(fit2, fit.measures = T, standardized = T)
 #model3
-model3 <- 'score ~ mean_heart + match_name_id'
-fit3 <- sem(model = model3, estimator = "ML", missing = "fiml",
-            data = mean_dat, meanstructure = T)
+model3 <- 'level: 1
+            score ~ mean_heart
+            level: 2
+            score
+            '
+fit3 <- sem(model = model1, estimator = "ML", missing = "fiml",
+            cluster = "match_name_id", data = mean_dat, meanstructure = T)
 summary(fit3, fit.measures = T, standardized = T)
 #model4
 model4 <- 'score ~ mean_heart  + set_number + shoot_order + countdown + arrows'
