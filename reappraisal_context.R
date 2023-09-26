f7c98f1fb98215775b3ece419af731b0f42e43bb
Chae Reen Kang <cherish4@g.skku.edu>
2023-09-26 10:27:32 +0900

    코드 정리

--- reappraisal_context.R
+++ reappraisal_context.R
@@ -1,9 +1,12 @@
+#install.packages("lmerTest")
 library(tidyverse);library(psych);library(tidyr);library(sem); library(lavaan)
+library(nlme);library(lme4);library(lmerTest)
 wisdom <- read_csv("HainesEtAl_data.csv")
 head(wisdom)
 as.data.frame(wisdom)
 wisdom %>% select(SEMA_ID, Time, CTRLcln, REAP, REAP_1, ZBFI_N, ZDASS_D, ZDASS_A,
                   ZDASS_S, ZRSE, ZSOCANX, CTRL_M) -> main_wis
+#multilevel SEM
 lmmod_D <- '
           level: 1
             REAP ~ 1 + p1*CTRLcln + p2*Time + p3*REAP_1
@@ -13,13 +16,10 @@ lmmod_D <- '
             Time ~ 1 + ZDASS_D + CTRL_M
             REAP_1 ~ 1 + ZDASS_D + CTRL_M'
 #lm_D <- sem(model = lmmod_D, data = main_wis, cluster = "SEMA_ID", verbose = T, 
-            optim.method = "em", em.iter.max = 10000, em.fx.tol = 1e-08, em.dx.tol = 1e-04) #수렴이 되지 않을 경우 em 알고리즘을 이용/ 10000이상으로 설정
+#            optim.method = "em", em.iter.max = 10000, em.fx.tol = 1e-08, em.dx.tol = 1e-04) #수렴이 되지 않을 경우 em 알고리즘을 이용/ 10000이상으로 설정
 #summary(lm_D)
 
-
 #lmer package로 해보기
-#install.packages("lmerTest")
-library(nlme);library(lme4);library(lmerTest)
 intercept.only.model <- lmer(REAP ~ 1 + ( 1 | SEMA_ID), data=main_wis) #fitting
 summary(intercept.only.model)
 ranef(intercept.only.model) #each regression for ID
@@ -36,20 +36,22 @@ summary(random.coefficients.model)
 ranova(random.coefficients.model)
 context.model <- lmer(REAP ~ CTRLcln + Time + REAP_1 + (CTRL_M + ZDASS_D | SEMA_ID), data = main_wis, REML = T)
 summary(context.model)
+
 main_wis %>% mutate(ID = as.factor(SEMA_ID)) -> main_wis
+
   ###lme function###
 intercept.only <- lme(REAP ~ 1,
-                      random = ~1 | ID,
+                      random = ~1 | SEMA_ID,
                       main_wis, method = "ML", na.action = na.omit)
 summary(intercept.only)
 VarCorr(intercept.only)#lme는 random 수준에서 표준화값만 나타남
 random.intercept <- lme(REAP ~ CTRLcln + Time + REAP_1,
-                        random = ~1 | ID,
+                        random = ~1 | SEMA_ID,
                         main_wis, method = "ML", na.action = na.omit)
 summary(random.intercept)
 VarCorr(random.intercept)
 random.coefficients <- lme(REAP ~  CTRLcln + Time + REAP_1,
-                           random = ~ CTRLcln + Time + REAP_1 | ID,
+                           random = ~ CTRLcln + Time + REAP_1 | SEMA_ID,
                            main_wis, method = "ML", na.action = na.omit)
 summary(random.coefficients)
 VarCorr(random.coefficients)
@@ -57,17 +59,17 @@ ranef(random.coefficients)["CTRLcln"] %>% round(2) %>% tail(20) #residuals of th
 anova(random.coefficients, random.intercept)
 #slope-as-outcome model
 slope.outcome <- lme(REAP ~  CTRLcln*ZDASS_D + Time*ZDASS_D + REAP_1*ZDASS_D + CTRLcln*CTRL_M + Time*CTRL_M + REAP_1*CTRL_M,
-                           random = ~ CTRLcln + Time + REAP_1 | ID,
+                           random = ~ CTRLcln + Time + REAP_1 | SEMA_ID,
                            main_wis, method = "ML", na.action = na.omit)
 summary(slope.outcome)
 VarCorr(slope.outcome)
 intercept.outcome <- lme(REAP ~ CTRLcln + Time + REAP_1 + ZDASS_D + CTRL_M,
-                     random = ~ CTRLcln + Time + REAP_1 | ID,
+                     random = ~ CTRLcln + Time + REAP_1 | SEMA_ID,
                      main_wis, method = "ML", na.action = na.omit)
 summary(intercept.outcome)
 VarCorr(intercept.outcome)
 intercept.slope.outcome <- lme(REAP ~  CTRLcln + Time + REAP_1 + ZDASS_D + CTRL_M + CTRLcln*ZDASS_D + Time*ZDASS_D + REAP_1*ZDASS_D + CTRLcln*CTRL_M + Time*CTRL_M + REAP_1*CTRL_M,
-                     random = ~ CTRLcln + Time + REAP_1 | ID,
+                     random = ~ CTRLcln + Time + REAP_1 | SEMA_ID,
                      main_wis, method = "ML", na.action = na.omit)
 summary(intercept.slope.outcome)
 anova(intercept.slope.outcome, intercept.outcome)
